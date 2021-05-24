3149950b62306f675af837c4d9311970aece7f4f
Kenneth Chew <72385483+kennethcw@users.noreply.github.com>
2021-05-24 14:48:29 +0200

    Test

--- Assets/Scenes/PlayerControls.cs
+++ Assets/Scenes/PlayerControls.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
 
-    Rigidbody2D rb;
-    float xInput;
-    public float speed;
+    Rigidbody2D rb; //v.1
+    float xInput; //v.1
+    public float speed; //v.1
 
-    private void Awake()
+    public GameObject bullet; //v.2
+    public Transform shootPosition; //v.2
+
+    private void Awake() //v.1
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //v.1
     }
 
     // Start is called before the first frame update
@@ -23,8 +26,18 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        xInput = Input.GetAxisRaw("Horizontal"); //v.1
 
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y); //v.1
+
+        if (Input.GetKeyDown("space")) //v.2
+        {
+            Fire(); //v.2
+        }
+
+        void Fire() //v.2
+        {
+            Instantiate(bullet, shootPosition.position, transform.rotation); //v.2
+        }
     }
 }
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float BulletSpeed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * BulletSpeed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
