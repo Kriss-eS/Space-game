a8acfda2e7f5259b6fbb67056df3524616c80ef0
JuicySoup <contactjonathanosterberg@gmail.com>
2021-05-25 13:41:39 +0200

    wall update

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    Rigidbody2D rb; //v.1
+    float xInput; //v.1
+    public float speed; //v.1
+
+    public GameObject bullet; //v.2
+    public Transform shootPosition; //v.2
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal"); //v.1
+
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y); //v.1
+
+
+        if (Input.GetKeyDown("space")) //v.2
+        {
+            Fire(); //v.2
+        }
+
+        void Fire() //v.2
+        {
+            Instantiate(bullet, shootPosition.position, transform.rotation); //v.2
+        }
+    }
+}
